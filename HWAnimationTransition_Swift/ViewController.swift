6c6143c58cf9c7e2af348914099e7c12a42e95f2
cerastes <chn_jin@163.com>
2016-03-20 19:13:13 +0800

    Initial Commit

---
+++ HWAnimationTransition_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HWAnimationTransition_Swift
+//
+//  Created by JOB on 16/3/20.
+//  Copyright © 2016年 mobi.wonders.ios.apps.cswhy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
